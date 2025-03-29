4123d26a4550dd46992472acbebf038bc037c796
Sennthx <63876124+Sennthx@users.noreply.github.com>
2025-03-29 19:53:09 +0100

    Added two routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,18 @@
 import { Routes } from '@angular/router';
 
-
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
+import { HomeComponent } from './pages/home/home.component';
+import { StudentsComponent } from './pages/students/students.component';
+import { AddressComponent } from './pages/address/address.component';
 
 export const routes: Routes = [
-
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      { path: '', component: HomeComponent },
+      { path: 'students', component: StudentsComponent },
+      { path: 'address', component: AddressComponent }
+    ]
+  }
 ];
