7b4b5891f63c24891c12330bd88ca3819ab8888c
Sennthx <63876124+Sennthx@users.noreply.github.com>
2025-03-29 19:51:16 +0100

    Changed scss to tailwind with daisyUI
    Needed to refactor after switching from angular material to daisyUI and tailwind css

--- frontend/src/app/layout/footer/footer.component.ts
+++ frontend/src/app/layout/footer/footer.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
+  standalone: true,
   imports: [],
   templateUrl: './footer.component.html',
-  styles: ``
-})
-export class FooterComponent {
+  styles: [`
 
-}
+  `]
+})
+export class FooterComponent { }
--- frontend/src/app/layout/header/header.component.ts
+++ frontend/src/app/layout/header/header.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule],
   templateUrl: './header.component.html',
-  styles: ``
-})
-export class HeaderComponent {
+  styles: [`
 
-}
+  `]
+})
+export class HeaderComponent { }
