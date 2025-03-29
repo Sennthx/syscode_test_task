7f9b97f82430d68db936b2f7b69e721c899fa3c4
Sennthx <63876124+Sennthx@users.noreply.github.com>
2025-03-29 19:52:21 +0100

    Modified main app component

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,29 +1,11 @@
-// src/app/app.config.ts
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideHttpClient } from '@angular/common/http';
-import { importProvidersFrom } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatInputModule } from '@angular/material/input';
-import { MatListModule } from '@angular/material/list';
-
-
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
-    provideAnimations(),
-    provideHttpClient(),
-    importProvidersFrom(
-      MatButtonModule,
-      MatCardModule,
-      MatInputModule,
-      MatListModule
-    )
+    provideRouter(routes),
+    provideAnimations()
   ]
 };
-
