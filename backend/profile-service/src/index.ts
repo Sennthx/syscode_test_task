36e492dae45f08e37f037769c602310ef261bce5
Sennthx <63876124+Sennthx@users.noreply.github.com>
2025-03-30 18:07:07 +0200

    Created basic endpoints

--- backend/profile-service/src/index.ts
+++ backend/profile-service/src/index.ts
@@ -1,3 +1,20 @@
-import "reflect-metadata";
+import express from 'express';
+import { StudentController } from './controllers/StudentController';
+import { AppDataSource } from './database/datasource';
 
-console.log("TypeScript is working!");
+const app = express();
+app.use(express.json());
+
+AppDataSource.initialize()
+    .then(() => {
+        const controller = new StudentController();
+
+        // Routes
+        app.get('/students', controller.getAllStudents);
+        app.post('/students', controller.createStudent);
+
+        app.listen(3000, () => {
+            console.log('Server running on http://localhost:3000');
+        });
+    })
+    .catch(console.error);
