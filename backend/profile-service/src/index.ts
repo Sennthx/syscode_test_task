865aa35b0153a99ae4d205f7cdafca8417154dd3
Sennthx <63876124+Sennthx@users.noreply.github.com>
2025-03-31 16:06:16 +0200

    Implemented logger middleware

--- backend/profile-service/src/index.ts
+++ backend/profile-service/src/index.ts
@@ -1,20 +1,38 @@
 import express from 'express';
 import { StudentController } from './controllers/StudentController';
 import { AppDataSource } from './database/datasource';
+import expressWinston from 'express-winston';
+import morgan from 'morgan';
+import logger from './logger'
 
 const app = express();
 app.use(express.json());
+
+app.use(morgan('combined', { stream: { write: (message) => logger.info(message.trim()) } }));
 
 AppDataSource.initialize()
     .then(() => {
         const controller = new StudentController();
 
-        // Routes
+        app.use(
+            expressWinston.logger({
+                winstonInstance: logger,
+                meta: true,
+                msg: 'HTTP {{req.method}} {{req.url}}',
+                expressFormat: true,
+                colorize: false,
+            })
+        );
+
         app.get('/students', controller.getAllStudents);
         app.post('/students', controller.createStudent);
+        app.put('/students/:id', controller.updateStudent);
+        app.delete('/students/:id', controller.deleteStudent);
 
         app.listen(3000, () => {
-            console.log('Server running on http://localhost:3000');
+            logger.info('Server running on http://localhost:3000');
         });
     })
-    .catch(console.error);
+    .catch((error) => {
+        logger.error('Error during Data Source initialization:', error);
+    });
