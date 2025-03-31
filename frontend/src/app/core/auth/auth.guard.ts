ac9a247119e776ce43a4d1b1007004bcf3866fe8
Sennthx <63876124+Sennthx@users.noreply.github.com>
2025-03-31 16:04:15 +0200

    Format/Update

--- frontend/src/app/core/auth/auth.guard.ts
+++ frontend/src/app/core/auth/auth.guard.ts
@@ -1,19 +1,18 @@
-// src/app/core/auth/auth.guard.ts
 import { CanActivate, Router } from '@angular/router';
 import { Injectable } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
-  constructor(private router: Router) {}
+  constructor(private router: Router) { }
 
   canActivate(): boolean {
-    const username = prompt('Username:'); // Replace with real auth later
+    const username = prompt('Username:');
     const password = prompt('Password:');
 
     if (username === 'admin' && password === 'secret') {
-      return true; // Allow access
+      return true;
     } else {
-      this.router.navigate(['/students']); // Redirect if failed
+      this.router.navigate(['/students']);
       return false;
     }
   }
