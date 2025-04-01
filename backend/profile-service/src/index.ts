20a98560c32976c1841f5fe391b4112d3af527d9
Sennthx <63876124+Sennthx@users.noreply.github.com>
2025-04-01 15:38:50 +0200

    Fix

--- backend/profile-service/src/index.ts
+++ backend/profile-service/src/index.ts
@@ -1,3 +1,4 @@
+import 'reflect-metadata';
 import express from 'express';
 import { StudentController } from './controllers/StudentController';
 import { AppDataSource } from './database/datasource';
